91a64e0d7509369c6797b2efbabfebc96b9d2d7d
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-08-16 19:10:03 +0300

    used macro accessor to replace the explicit getter and setter methods

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,13 @@
 ## code your solution here. 
+require 'pry'
+class Cat
+    attr_accessor :name
+    def meow
+        puts "meow!"
+    end
+end
+
+paka = Cat.new
+paka.name = "Paka"
+paka.meow
+# binding.pry
